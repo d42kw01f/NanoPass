12eb814ae52ce834d0656f1b8a4d54c1245a80e7
Nano Shadows <72387469+n4n05h4d0w5@users.noreply.github.com>
2020-11-03 21:15:29 +0530

    Add files via upload

---
+++ NanoPass.py
@@ -0,0 +1,39 @@
+import string 
+import random
+
+class CreatPasswords:
+    Letters = string.ascii_letters
+    Numbers = string.digits
+    SpecialChar = string.punctuation
+
+    def __init__(self, length, UserInput):
+        self.length = length
+        self.UserInput = UserInput
+
+    def TheMainOne(self):
+        if self.UserInput == 1:
+            TheConstant = CreatPasswords.Numbers + CreatPasswords.Letters + CreatPasswords.SpecialChar
+        elif self.UserInput == 2:
+            TheConstant = CreatPasswords.Numbers + CreatPasswords.Letters
+        elif self.UserInput == 3:
+            TheConstant = CreatPasswords.Letters
+        else:
+            raise ValueError('Wrong Input...')
+        TheConstant = list(TheConstant)
+        random.shuffle(TheConstant)
+        random_password = random.choices(TheConstant,  k=self.length)
+        TheRandPassword = ''.join(random_password)
+        return TheRandPassword
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            TheLen = int(input("Enter the length: "))
+            UserOption = int(input("Enter the options: "))
+            The_Pass = CreatPasswords(TheLen, UserOption)
+            ThePassword = CreatPasswords.TheMainOne(The_Pass)
+            break
+        except ValueError as e:
+            print("Opps: Something went wrong!!\nLet's try this again...\n\n")
+    print("You are Password is: \n{}".format(ThePassword))
